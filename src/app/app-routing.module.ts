13067e0067e5ae76ece48acf296ffe6514ba1245
Abel Paz <abellpazz12@gmail.com>
2021-01-05 07:58:05 -0500

    Agregando las rutas al App-routingmodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/Home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'Home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'Product',
+        loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+      },
+      {
+        path: 'Contact',
+        loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
+      },
+    ]
+  },
+  {
+    path: 'Admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  //escoja dinamicamente una estrategia de precarga
+  imports: [RouterModule.forRoot(routes,{
+   preloadingStrategy:PreloadAllModules 
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
 import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
     LayoutComponent
   ],
   imports: [
