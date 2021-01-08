3b9ef662634c5a7470b69eeacd6dc534ba0b8c66
Luis Ramos <luismeramosr@gmail.com>
2021-01-07 19:00:54 -0500

    Agregando assets

---
+++ src/app/core/models/Product.ts
@@ -0,0 +1,7 @@
+export class Product{
+    id?:string;
+    image?:string;
+    title?:string;
+    price?:number;
+    description?:string;
+}
---
+++ src/app/modules/home/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit,AfterViewInit } from '@angular/core';
+import {Swiper} from 'swiper';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass']
+})
+export class HomeComponent implements OnInit {
+
+mySwiper ! :Swiper;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    this.mySwiper= new Swiper('.swiper-container')
+  }
+}
