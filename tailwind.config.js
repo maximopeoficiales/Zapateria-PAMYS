998079a8b72a382946dbdcdc600f53692b682c09
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-02-01 02:52:38 -0500

    se agrego purgador

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  purge: [
+  purge: {
+    enabled:true,
+    content:[
     './src/**/*.html',
-  ],
+  ]},
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
