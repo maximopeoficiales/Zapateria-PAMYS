2728a9f1d5ff89d3f3df8c73e977708f826ab881
Luis Ramos <luismeramosr@gmail.com>
2021-02-17 19:38:37 -0500

    Agregando ejemplo de busqueda

--- src/app/pages/my-account/my-account-routing.module.ts
+++ src/app/pages/my-account/my-account-routing.module.ts
@@ -2,60 +2,60 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
 import {MyAccountShowComponent} from './subpages/my-account-show/my-account-show.component';
-import { MyAccountComponent } from './components/my-account/my-account.component';
-import { MyAccountEditComponent } from './subpages/my-account-edit/my-account-edit.component';
-import { OrderListComponent } from './subpages/orders/order-list/order-list.component';
-import { VoucherDetailComponent } from './subpages/voucher/voucher-detail/voucher-detail.component';
+import {MyAccountComponent} from './components/my-account/my-account.component';
+import {MyAccountEditComponent} from './subpages/my-account-edit/my-account-edit.component';
+import {OrderListComponent} from './subpages/orders/order-list/order-list.component';
+//import { VoucherDetailComponent } from './subpages/voucher/voucher-detail/voucher-detail.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: MyAccountComponent,
-    children: [
-      {
+    {
         path: '',
-        redirectTo: 'profile',
-        pathMatch: 'full'
-      },
-      {
-        path: 'profile',
-        component: MyAccountShowComponent
-      },
-      {
-        path: 'edit',
-        component: MyAccountEditComponent
-      },
-      {
-        path: 'orders',
-        component:  OrderListComponent
-      },
-      {
-        path: 'voucher',
-        component:  VoucherDetailComponent
-      }
-      // {
-      //   path: 'vendors',
-      //   component: VendorListComponent,
-      // },
-      // {
-      //   path: 'vendors/form',
-      //   component: VendorDetailComponent,
-      // },
-      // {
-      //   path: 'vendors/form/:id',
-      //   component: VendorDetailComponent,
-      // },
-      // {
-      //   path: '',
-      //   redirectTo: 'categorys',
-      //   pathMatch: 'full',
-      // },
-    ],    
-  }  
+        component: MyAccountComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'profile',
+                pathMatch: 'full'
+            },
+            {
+                path: 'profile',
+                component: MyAccountShowComponent
+            },
+            {
+                path: 'edit',
+                component: MyAccountEditComponent
+            },
+            {
+                path: 'orders',
+                component: OrderListComponent
+            },
+            //     {
+            //     path: 'voucher',
+            //    component:  VoucherDetailComponent
+            // }
+            // {
+            //   path: 'vendors',
+            //   component: VendorListComponent,
+            // },
+            // {
+            //   path: 'vendors/form',
+            //   component: VendorDetailComponent,
+            // },
+            // {
+            //   path: 'vendors/form/:id',
+            //   component: VendorDetailComponent,
+            // },
+            // {
+            //   path: '',
+            //   redirectTo: 'categorys',
+            //   pathMatch: 'full',
+            // },
+        ],
+    }
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [CommonModule, RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class MyAccountRoutingModule {}
--- src/app/pages/my-account/my-account.module.ts
+++ src/app/pages/my-account/my-account.module.ts
@@ -6,28 +6,28 @@ import {MyAccountRoutingModule} from './my-account-routing.module';
 import {MyAccountShowComponent} from './subpages/my-account-show/my-account-show.component';
 import {MyAccountComponent} from './components/my-account/my-account.component';
 import {HttpClientModule} from '@angular/common/http';
-import { MyAccountEditComponent } from './subpages/my-account-edit/my-account-edit.component';
-import { OrderListComponent } from './subpages/orders/order-list/order-list.component';
-import { VoucherDetailComponent } from './subpages/voucher/voucher-detail/voucher-detail.component';
-import { MaterialModule } from 'src/app/core/modules/material/material.module';
+import {MyAccountEditComponent} from './subpages/my-account-edit/my-account-edit.component';
+import {OrderListComponent} from './subpages/orders/order-list/order-list.component';
+//import { VoucherDetailComponent } from './subpages/voucher/voucher-detail/voucher-detail.component';
+import {MaterialModule} from 'src/app/core/modules/material/material.module';
 
 @NgModule({
-  declarations: [
-    MyAccountShowComponent, 
-    MyAccountComponent, 
-    MyAccountEditComponent,
-    MyAccountShowComponent,
-    MyAccountComponent,
-    OrderListComponent,
-    VoucherDetailComponent,
-  ],
-  imports: [
-    CommonModule,
-    MyAccountRoutingModule,
-    SharedModule,
-    FormsModule,
-    HttpClientModule,
-    MaterialModule
-  ],
+    declarations: [
+        MyAccountShowComponent,
+        MyAccountComponent,
+        MyAccountEditComponent,
+        MyAccountShowComponent,
+        MyAccountComponent,
+        OrderListComponent,
+        // VoucherDetailComponent,
+    ],
+    imports: [
+        CommonModule,
+        MyAccountRoutingModule,
+        SharedModule,
+        FormsModule,
+        HttpClientModule,
+        MaterialModule
+    ],
 })
 export class MyAccountModule {}
