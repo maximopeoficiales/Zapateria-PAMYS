71d1910fa89c52fe8fd538ed7abd8d60b6b7ec03
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-02-23 00:34:17 -0500

    busqueda de pedidos por select

--- src/app/pages/admin/subpages/order/order-list/order-list.component.ts
+++ src/app/pages/admin/subpages/order/order-list/order-list.component.ts
@@ -1,195 +1,214 @@
-import {HtmlAstPath} from '@angular/compiler';
-import {Component, OnInit, OnChanges, SimpleChange} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {Client, Order, OrderStatus, Product} from 'src/app/core/api/models';
+import { HtmlAstPath } from '@angular/compiler';
 import {
-    ClientControllerService,
-    OrderControllerService,
-    OrderStatusControllerService,
+  Component,
+  OnInit,
+  OnChanges,
+  SimpleChange,
+  ValueSansProvider,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Client, Order, OrderStatus, Product } from 'src/app/core/api/models';
+import {
+  ClientControllerService,
+  OrderControllerService,
+  OrderStatusControllerService,
 } from 'src/app/core/api/services';
 // import * as html2pdf from 'html2pdf.js/types/index';
-import {jsPDF} from 'jspdf';
+import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
-import {environment} from 'src/environments/environment';
-import {LoginService} from 'src/app/core/services/auth/login/login.service';
+import { environment } from 'src/environments/environment';
+import { LoginService } from 'src/app/core/services/auth/login/login.service';
 
 @Component({
-    selector: 'app-order-list',
-    templateUrl: './order-list.component.html',
-    styleUrls: ['./order-list.component.sass'],
+  selector: 'app-order-list',
+  templateUrl: './order-list.component.html',
+  styleUrls: ['./order-list.component.sass'],
 })
 export class OrderListComponent implements OnInit {
-    orders: Order[] = [];
-    order: Order = {};
-    users: Client[] = [];
-    orderStatuses: OrderStatus[] = [];
-    showModalDetail: boolean = false;
-    selectSearcher: FormControl = new FormControl();
-    selectedStatus: string = '';
-    dateCreated: string = '';
-    editDate: boolean = false;
-    editStatus: boolean = false;
-    isPrinting: boolean = false;
-    loading: boolean = true;
-    productImagesUrl: string = environment.url_products_images;
+  orders: Order[] = [];
+  order: Order = {};
+  users: Client[] = [];
+  orderStatuses: OrderStatus[] = [];
+  showModalDetail = false;
+  selectSearcher: FormControl = new FormControl();
+  selectedStatus = '';
+  dateCreated = '';
+  editDate = false;
+  editStatus = false;
+  isPrinting = false;
+  loading = true;
+  productImagesUrl: string = environment.url_products_images;
 
-    isFirstLoad: boolean = true;
-    filterFunction: any;
-    user: Client = {};
-    statusSelected: string = '';
+  isFirstLoad = true;
+  filterFunction: any;
+  user: Client = {};
+  statusSelected = '';
 
-    value = '';
-    constructor(
-        private orderService: OrderControllerService,
-        private orderStatusesService: OrderStatusControllerService,
-        private usersService: ClientControllerService,
-        private clientService: LoginService
-    ) {
-        this.user = clientService.getUser()!;
-    }
+  value = '';
+  constructor(
+    private orderService: OrderControllerService,
+    private orderStatusesService: OrderStatusControllerService,
+    private usersService: ClientControllerService,
+    private clientService: LoginService
+  ) {
+    this.user = clientService.getUser();
+  }
 
-    ngOnInit(): void {
-        if (this.isFirstLoad) {
-            setTimeout(() => {
-                this.orderService.getAllUsingGET3().subscribe((orders) => {
-                    this.orders = orders;
-                    this.orderStatusesService.getAllUsingGET4().subscribe(statuses => {
-                        this.orderStatuses = statuses;
-                    });
-                    this.loading = false;
-                });
-            }, 300);
-        }
-        this.isFirstLoad = false;
-    }
-
-    getImageUrl(product: Product): string {
-        return this.productImagesUrl + product.thumbnailUrl;
-    }
-
-    toggleEditDate() {
-        this.editDate = !this.editDate;
-    }
-
-    toggleEditStatus() {
-        this.editStatus = !this.editStatus;
-        let newStatusID = this.orderStatuses.find(
-            (e) => e.status == this.selectedStatus
-        )?.idOrderStatus;
-        this.order.idOrderStatus = newStatusID;
-    }
-
-    togglePrintState() {
-        this.editStatus = false;
-        this.editDate = false;
-        this.isPrinting = !this.isPrinting;
-    }
-
-    getUserImageURL(order: Order): string {
-        return environment.url_client_images + order.client!.profilePictureUrl;
-    }
-
-    showDetail(order: Order) {
-        this.order = order;
-        this.selectedStatus = order.orderStatus?.status!;
-        this.dateCreated = order.dateCreated!;
-        this.editDate = false;
-        this.editStatus = false;
-        this.showModalDetail = true;
-    }
-
-    closeDetail() {
-        this.showModalDetail = false;
-    }
-
-    getSubTotalRow(price: number, quantity: number) {
-        return price * quantity;
-    }
-
-    getSubTotal(): number {
-        return this.order.products!.reduce(
-            (i, j) => i + j.product!.price! * j.quantity!,
-            0
-        );
-    }
-
-    getIgv(): number {
-        return this.getSubTotal() * 0.18;
-    }
-
-    getTotal() {
-        return this.getSubTotal() * 1.18;
-    }
-
-    formatDate() {
-        let res = this.dateCreated.replace('/', '-') + 'T12:00:00Z';
-        // console.log(res);
-        return res;
-    }
-
-    updateOrder() {
-        let newStatusID = this.orderStatuses.find(
-            (e) => e.status == this.selectedStatus
-        )?.idOrderStatus;
-        this.order.idOrderStatus = newStatusID;
-        this.order.igv = this.getIgv();
-        this.order.total = this.getTotal();
-        this.order.subtotal = this.getSubTotal();
-        this.order.dateCreated = this.dateCreated == this.order.dateCreated ? this.order.dateCreated + "Z" : this.formatDate();
-        this.order.orderStatus = this.orderStatuses.find(e => e.idOrderStatus == newStatusID);
-        console.log(this.order);
-        this.orderService.updateUsingPUT3(this.order).subscribe(() => {
-            this.closeDetail();
-            this.ngOnInit();
+  ngOnInit(): void {
+    if (this.isFirstLoad) {
+      setTimeout(() => {
+        this.orderService.getAllUsingGET3().subscribe((orders) => {
+          this.orders = orders;
+          this.orderStatusesService.getAllUsingGET4().subscribe((statuses) => {
+            this.orderStatuses = statuses;
+          });
+          this.loading = false;
         });
+      }, 300);
     }
+    this.isFirstLoad = false;
+  }
 
-    printOrder() {
+  getImageUrl(product: Product): string {
+    return this.productImagesUrl + product.thumbnailUrl;
+  }
+
+  toggleEditDate(): void {
+    this.editDate = !this.editDate;
+  }
+
+  toggleEditStatus(): void {
+    this.editStatus = !this.editStatus;
+    const newStatusID = this.orderStatuses.find(
+      (e) => e.status === this.selectedStatus
+    )?.idOrderStatus;
+    this.order.idOrderStatus = newStatusID;
+  }
+
+  togglePrintState(): void {
+    this.editStatus = false;
+    this.editDate = false;
+    this.isPrinting = !this.isPrinting;
+  }
+
+  getUserImageURL(order: Order): string {
+    // tslint:disable-next-line: no-non-null-assertion
+    return environment.url_client_images + order.client!.profilePictureUrl;
+  }
+
+  showDetail(order: Order): void {
+    this.order = order;
+    // tslint:disable-next-line: no-non-null-assertion
+    this.selectedStatus = order.orderStatus?.status!;
+    // tslint:disable-next-line: no-non-null-assertion
+    this.dateCreated = order.dateCreated!;
+    this.editDate = false;
+    this.editStatus = false;
+    this.showModalDetail = true;
+  }
+
+  closeDetail(): void {
+    this.showModalDetail = false;
+  }
+
+  getSubTotalRow(price: number, quantity: number): number {
+    return price * quantity;
+  }
+
+  getSubTotal(): number {
+    // tslint:disable-next-line: no-non-null-assertion
+    return this.order.products!.reduce(
+      // tslint:disable-next-line: no-non-null-assertion
+      (i, j) => i + j.product!.price! * j.quantity!,
+      0
+    );
+  }
+
+  getIgv(): number {
+    return this.getSubTotal() * 0.18;
+  }
+
+  getTotal(): number {
+    return this.getSubTotal() * 1.18;
+  }
+
+  formatDate(): string {
+    const res = this.dateCreated.replace('/', '-') + 'T12:00:00Z';
+    // console.log(res);
+    return res;
+  }
+
+  updateOrder(): void {
+    const newStatusID = this.orderStatuses.find(
+      (e) => e.status === this.selectedStatus
+    )?.idOrderStatus;
+    this.order.idOrderStatus = newStatusID;
+    this.order.igv = this.getIgv();
+    this.order.total = this.getTotal();
+    this.order.subtotal = this.getSubTotal();
+    this.order.dateCreated =
+      this.dateCreated === this.order.dateCreated
+        ? this.order.dateCreated + 'Z'
+        : this.formatDate();
+    this.order.orderStatus = this.orderStatuses.find(
+      (e) => e.idOrderStatus === newStatusID
+    );
+    console.log(this.order);
+    this.orderService.updateUsingPUT3(this.order).subscribe(() => {
+      this.closeDetail();
+      this.ngOnInit();
+    });
+  }
+
+  printOrder(): void {
+    this.togglePrintState();
+    const data = document.getElementById('orderDetail');
+    // tslint:disable-next-line: no-non-null-assertion
+    html2canvas(data!)
+      .then((canvas) => {
+        const imgWidth = 210;
+        const pageHeight = 295;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        const heightLeft = imgHeight;
+        const contentDataURL = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+        const position = 0;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+        pdf.save(`Pedido-${this.order.idOrder}.pdf`); // Generated PDF
+      })
+      .then(() => {
         this.togglePrintState();
-        let data = document.getElementById('orderDetail');
-        html2canvas(data!)
-            .then((canvas) => {
-                var imgWidth = 210;
-                var pageHeight = 295;
-                var imgHeight = (canvas.height * imgWidth) / canvas.width;
-                var heightLeft = imgHeight;
-                const contentDataURL = canvas.toDataURL('image/png');
-                let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
-                var position = 0;
-                pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-                pdf.save(`Pedido-${this.order.idOrder}.pdf`); // Generated PDF
-            })
-            .then(() => {
-                this.togglePrintState();
-            });
-    }
+      });
+  }
 
-    changedate(date: string) {
-        this.loading = true;
-        this.filterFunction = setTimeout(() => {
-            this.orderService.getAllUsingGET3().subscribe((orders) => {
-                this.orders = orders.filter(e => e.dateCreated?.match(date));
-                this.loading = false;
-                this.ngOnInit();
-            });
-        }, 300);
-    }
+  changedate(date: string): void {
+    this.loading = true;
+    this.filterFunction = setTimeout(() => {
+      this.orderService.getAllUsingGET3().subscribe((orders) => {
+        this.orders = orders.filter((e) => e.dateCreated?.match(date));
+        this.loading = false;
+        this.ngOnInit();
+      });
+    }, 300);
+  }
 
-    filterByStatus(estado: string) {
-        this.loading = true;
-        this.filterFunction = setTimeout(() => {
-            this.orderService.getAllUsingGET3().subscribe((orders) => {
+  filterByStatus(estado: string): void {
+    this.loading = true;
+    this.filterFunction = setTimeout(() => {
+      this.orderService.getAllUsingGET3().subscribe((orders) => {
+        if (estado === 'Todos') {
+          this.orders = orders;
+        } else {
+          this.orders = orders.filter((e) =>
+            // tslint:disable-next-line: no-non-null-assertion
+            e.orderStatus?.status!.match(estado)
+          );
+        }
 
-                if (estado == "Todos") {
-                    this.orders = orders;
-                } else {
-                    this.orders = orders.filter(e => e.orderStatus?.status!.match(estado));
-                }
-
-                this.loading = false;
-                this.ngOnInit();
-            });
-        }, 300);
-    }
+        this.loading = false;
+        this.ngOnInit();
+      });
+    }, 300);
+  }
 }
--- src/app/pages/my-account/subpages/orders/order-list/order-list.component.ts
+++ src/app/pages/my-account/subpages/orders/order-list/order-list.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Client, Order } from 'src/app/core/api/models';
-import { OrderControllerService } from 'src/app/core/api/services';
+import {
+  OrderControllerService,
+  OrderStatusControllerService,
+} from 'src/app/core/api/services';
 import { LoginService } from 'src/app/core/services/auth/login/login.service';
 import { environment } from 'src/environments/environment';
+import { OrderStatus } from '../../../../../core/api/models/order-status';
 
 @Component({
   selector: 'app-order-list',
@@ -11,27 +15,34 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./order-list.component.sass'],
 })
 export class OrderListComponent implements OnInit {
-  loading: boolean = true;
-  orders: Order[] = [];
-  user: Client = {};
-  imagesUrl: string = environment.url_client_images;
-  statusSelected: string = '';
-  filterFunction: any;
-
   constructor(
     private orderService: OrderControllerService,
+    private orderStatusService: OrderStatusControllerService,
     private clientService: LoginService,
     private router: Router
   ) {
-    this.user = clientService.getUser()!;
+    this.user = this.clientService.getUser();
   }
+  loading = true;
+  orders: Order[] = [];
+  orderStatuses: OrderStatus[] = [];
+  user: Client = {};
+  imagesUrl: string = environment.url_client_images;
+  statusSelected = '';
+  filterFunction: any;
+
+  datenow: any;
 
   ngOnInit(): void {
-    if (this.orders.length == 0) {
+    if (this.orders.length === 0) {
       setTimeout(() => {
         this.orderService.getAllUsingGET3().subscribe((orders) => {
-          this.orders = orders.filter((o) => o.idClient == this.user.idClient);
+          this.orders = orders.filter((o) => o.idClient === this.user.idClient);
           this.loading = false;
+        });
+
+        this.orderStatusService.getAllUsingGET4().subscribe((status) => {
+          this.orderStatuses = status;
         });
       }, 300);
     }
@@ -41,42 +52,60 @@ export class OrderListComponent implements OnInit {
     return this.imagesUrl + this.user.profilePictureUrl;
   }
 
-  uploadVoucher() {
+  uploadVoucher(): void {
     this.router.navigate(['/my-account/voucher']);
   }
-
-  datenow: any;
-  changedate(date: string) {
+  changedate(date: string): void {
     date += 'T00:00:00';
     console.log(date);
     this.orderService.getAllUsingGET3().subscribe((orders) => {
+      console.log(orders);
       this.orders = orders
-        .filter((o) => o.idClient == this.user.idClient)
-        .filter((e) => e.dateCreated == date);
+        .filter((o) => o.idClient === this.user.idClient)
+        .filter((e) => e.dateCreated === date);
 
       console.log(this.orders);
 
-      if (this.orders.length == 0) {
+      if (this.orders.length === 0) {
+        // tslint:disable-next-line: no-shadowed-variable
         this.orderService.getAllUsingGET3().subscribe((orders) => {
-          this.orders = orders.filter((e) => e.idClient == this.user.idClient);
+          this.orders = orders.filter((e) => e.idClient === this.user.idClient);
         });
       }
     });
   }
-
-  filterByStatus() {
-    clearTimeout(this.filterFunction);
+  filterByStatus(estado: string): void {
     this.loading = true;
     this.filterFunction = setTimeout(() => {
       this.orderService.getAllUsingGET3().subscribe((orders) => {
-        this.ngOnInit();
-        this.orders = orders.filter(
-          (o) =>
-            o.idClient == this.user.idClient &&
-            o.orderStatus?.status! == this.statusSelected
-        );
+        if (estado === 'Todos') {
+          this.orders = orders.filter((o) => o.idClient === this.user.idClient);
+        } else {
+          this.orders = orders.filter(
+            (o) =>
+              o.idClient === this.user.idClient &&
+              // tslint:disable-next-line: no-non-null-assertion
+              o.orderStatus?.status! === estado
+          );
+        }
+
         this.loading = false;
       });
     }, 300);
   }
+  // filterByStatus() {
+  //   clearTimeout(this.filterFunction);
+  //   this.loading = true;
+  //   this.filterFunction = setTimeout(() => {
+  //     this.orderService.getAllUsingGET3().subscribe((orders) => {
+  //       this.ngOnInit();
+  //       this.orders = orders.filter(
+  //         (o) =>
+  //           o.idClient == this.user.idClient &&
+  //           o.orderStatus?.status! == this.statusSelected
+  //       );
+  //       this.loading = false;
+  //     });
+  //   }, 300);
+  // }
 }
